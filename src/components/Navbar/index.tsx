f244ba9850d0e4a47414ee3a2fc8f79f74f47cea
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-03-26 23:51:53 +0800

    WIP: navbar links - create initial link component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,7 +9,35 @@ export default function Navbar({}: Props) {
 
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}></div>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img src={Logo} alt="evogym logo" />
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <a href="#">
+                  <li>Home</li>
+                </a>
+                <a href="#">
+                  <li>Benefits</li>
+                </a>
+                <a href="#">
+                  <li>Our Classes</li>
+                </a>
+                <a href="#">
+                  <li>Contact Us</li>
+                </a>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <a href="#">
+                  <li>Sign In</li>
+                </a>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
