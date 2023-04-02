83fa3736e43465d82f0ef8c4920fc0ed7dcbb0b9
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-04-02 21:28:40 +0800

    WIP: add action button to navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,6 +4,7 @@ import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
 import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -47,7 +48,9 @@ export default function Navbar({ selectedPage, setSelectedPage }: Props) {
                 </div>
                 <div className={`${flexBetween} gap-8`}>
                   <p>Sign In</p>
-                  <button>Become a Member</button>
+                  <ActionButton setSelectedPage={setSelectedPage}>
+                    Become a Member
+                  </ActionButton>
                 </div>
               </div>
             ) : (
